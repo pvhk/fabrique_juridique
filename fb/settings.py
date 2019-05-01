42f4d318ef859bf68fddfdc4c2b132719e8a4fbc
paulvanhecke <p.vanhecke86@gmail.com>
2019-05-01 23:28:11 +0200

    run produuctttion

--- fb/settings.py
+++ fb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '71k4_gt6r!kh215j)lfa7xvjcx4c*9=akwza!^44dgot_yunjb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
